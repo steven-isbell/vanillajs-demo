f8c805bc95f4da0270ebc533db5160738d8ce961
Steven Isbell <steven.isbell18@gmail.com>
2018-05-02 13:31:54 -0500

    added header styles

--- js/components/Header.js
+++ js/components/Header.js
@@ -2,13 +2,18 @@ const Header = () => {
   const elem = document.createElement('header');
   elem.style.marginBottom = '15px';
   elem.innerHTML = `
-                    <ul>
+                    <ul style="display:flex;justify-content:space-between;align-items:center;padding:10px;">
                       <li>
-                        <a href="#home">Home</a>
+                        <img height="50" width="50" src="http://logodust.com/img/free/logo1.png" />
                       </li>
-                      <li>
-                        <a href="#characters">Characters</a>
-                      </li>
+                      <div style="display:flex;align-items:center;justify-content:space-between;width:20%;max-width:150px;">
+                        <li>
+                          <a href="#home">Home</a>
+                        </li>
+                        <li>
+                          <a href="#characters">Characters</a>
+                        </li>
+                      </div>
                     </ul>
                   `;
   return elem;
