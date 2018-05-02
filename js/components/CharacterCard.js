23dd95c7e565cdbe6704bdc201931a88ce80c1de
Steven Isbell <steven.isbell18@gmail.com>
2018-05-02 13:43:15 -0500

    added event listeners

--- js/components/CharacterCard.js
+++ js/components/CharacterCard.js
@@ -7,9 +7,17 @@ const CharCard = char => {
     width: 300px;
     height: 350px;
     align-items: center;
-    justify-content:center;`
+    justify-content:center;
+    box-shadow: 2px 2px 12px 2px rgb(0, 0, 0, .25);
+    margin: 10px`
   );
   elem.innerHTML = `<h1 style="margin-bottom:10px">${char.name}</h1>
                     <p>${char.birth_year}</p>`;
+  elem.addEventListener('mouseenter', () => {
+    elem.style.transform = 'scale(1.1)';
+  });
+  elem.addEventListener('mouseleave', () => {
+    elem.style.transform = 'scale(1)';
+  });
   return elem;
 };
