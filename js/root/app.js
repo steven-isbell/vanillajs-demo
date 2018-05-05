2d7273ae51341502087981d981715a5425d232bd
Steven Isbell <steven.isbell18@gmail.com>
2018-05-05 17:07:33 -0600

    added new case

--- js/root/app.js
+++ js/root/app.js
@@ -1,6 +1,12 @@
 // App is deciding what content to render
-const App = async url => {
+const App = async (url, id) => {
+  const hasParam = location.hash.split('/')[1];
   switch (location.hash) {
+    case `#character/${hasParam}`:
+      const char = cache.people.results.find(val =>
+        val.name.toLowerCase().includes(hasParam.toLowerCase())
+      );
+      return CharContainer(char);
     case '#characters':
       const chars = await getChars(url);
       return CharContainer(chars);
