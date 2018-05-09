e41e43b314946a52624d0d95ba6934b8b2eaeee5
Steven Isbell <steven.isbell18@gmail.com>
2018-05-09 09:50:57 -0500

    async

--- js/root/index.js
+++ js/root/index.js
@@ -2,15 +2,14 @@
 const renderElem = document.getElementById('app');
 
 // The content to render to the DOM
-const renderContent = url => {
-  return App(url).then(val => {
-    const prev = renderElem.childNodes[0] || null;
-    if (prev && val) {
-      return renderElem.replaceChild(val, prev);
-    } else if (val) {
-      renderElem.appendChild(val);
-    }
-  });
+const renderContent = async url => {
+  const elem = await App(url);
+  const prev = renderElem.childNodes[0] || null;
+  if (prev && elem) {
+    return renderElem.replaceChild(elem, prev);
+  } else if (elem) {
+    return renderElem.appendChild(elem);
+  }
 };
 
 // initial load from utils
